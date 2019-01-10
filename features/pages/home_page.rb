b2c357a09a91ee83f2042f4cb72c36576cf3527d
sergeytdl <41524278+sergeytdl@users.noreply.github.com>
2019-01-10 08:41:58 +0200

    exam1

--- features/pages/home_page.rb
+++ features/pages/home_page.rb
@@ -7,7 +7,7 @@ class HomePage < BasePage
     @find_details1 = Element.new(:css, '#productBlock5458958 > div > div > div.product-options.fields-description > div')
     @find_details2 = Element.new(:css, '#productBlock4706952 > div > div > div.product-options.fields-description > div:nth-child(1)')
     @find_details3 = Element.new(:css, '#productBlock4706946 > div > div > p > a')
-    @find_price1 = Element.new(:xpath, '//*[@id="productBlock5458958"]/div/div/div[2]/span[1][contains(text(), "€ 176,99")]')
+    @find_price1 = Element.new(:xpath, '//*[@id="productBlock5458958"]/div/div/div[2]/span[1][contains(text(), "€ 176,977")]')
     @find_price2 = Element.new(:xpath, '//*[@id="productBlock4706952"]/div/div/div[2]/span[1][contains(text(), "€ 109,99")]')
     @find_price3 = Element.new(:xpath, '//*[@id="productBlock4706946"]/div/div/div[2]/span[1][contains(text(), "€ 97,78")]')
 end
